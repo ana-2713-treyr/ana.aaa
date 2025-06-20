f7fea88a3b75346f79ac19bb0f0958454e4b34b3
ana-2713-treyr <ts24001@ts.hs.kr>
2025-06-20 03:09:10 +0000

    111

--- app_eda.py
+++ app_eda.py
@@ -234,7 +234,7 @@ class EDA:
 
         # ----- 영어 지역명 매핑 -----
         kor2eng = {
-            "서울":"서울","부산":"Busan","대구":"Daegu","인천":"Incheon","광주":"Gwangju",
+            "서울":"seoul","부산":"Busan","대구":"Daegu","인천":"Incheon","광주":"Gwangju",
             "대전":"Daejeon","울산":"Ulsan","세종":"Sejong","경기":"Gyeonggi","강원":"Gangwon",
             "충북":"Chungbuk","충남":"Chungnam","전북":"Jeonbuk","전남":"Jeonnam",
             "경북":"Gyeongbuk","경남":"Gyeongnam","제주":"Jeju","전국":"National"
